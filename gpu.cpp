792055bd2c882de657427fec36f533d7dbedddbe
SEL <sel@devops1.sel>
2019-11-05 15:32:35 +0100

    Overhaul

--- gpu.cpp
+++ gpu.cpp
@@ -1,3 +1,6 @@
+#include <cstdint>
+#include <string>
+#include <vector>
 #include <cstdlib>
 #include <cstring>
 #ifdef __APPLE__
@@ -6,25 +9,47 @@
 #include <CL/cl.h>
 #endif
 
+namespace gpu {
+	enum class vendor_t {
+		intel,
+		amd,
+		nvidia,
+		microsoft,
+		qualcomm,
+		unknown,
+	};
+	struct device_properties_t {
+		vendor_t vendor;
+		std::string name;
+		std::size_t memory_size;
+		std::size_t cache_size;
+		std::uint64_t max_frequency;
+	};
 
-static gpuinfo::gpu::vendor_t parse_vendor(const char* name) {
+
+	/// Returns all GPU's properties.
+	std::vector<device_properties_t> device_properties(cl_device_id device);
+}  // namespace gpu
+
+static gpu::vendor_t parse_vendor(const char* name) {
 	if(!std::strcmp(name, "Intel(R) Corporation"))
-		return gpuinfo::gpu::vendor_t::intel;
+		return gpu::vendor_t::intel;
 	else if(!std::strcmp(name, "Advanced Micro Devices, Inc."))
-		return gpuinfo::gpu::vendor_t::amd;
+		return gpu::vendor_t::amd;
 	else if(!std::strcmp(name, "NVIDIA Corporation"))
-		return gpuinfo::gpu::vendor_t::nvidia;
+		return gpu::vendor_t::nvidia;
 	else
-		return gpuinfo::gpu::vendor_t::unknown;
+		return gpu::vendor_t::unknown;
 }
 
 
-std::vector<gpuinfo::gpu::device_properties_t> gpuinfo::gpu::device_properties(cl_uint device) {
+std::vector<gpu::device_properties_t> gpu::device_properties(cl_device_id device) {
 	char name[256];
 	char vendorname[256];
 	cl_ulong cache;
 	cl_ulong memory;
 	cl_uint max_frequency;
+	std::vector<gpu::device_properties_t> ret;
 
 	clGetDeviceInfo(device, CL_DEVICE_GLOBAL_MEM_CACHE_SIZE, sizeof(cache), &cache, nullptr);
 	clGetDeviceInfo(device, CL_DEVICE_MAX_CLOCK_FREQUENCY, sizeof(max_frequency), &max_frequency, nullptr);
@@ -36,3 +61,4 @@ std::vector<gpuinfo::gpu::device_properties_t> gpuinfo::gpu::device_properties(c
 	
 	return ret;
 }
+
--- gpu.h
+++ gpu.h
@@ -1,30 +1,26 @@
-#pragma once
-
 #include <cstdint>
 #include <string>
 #include <vector>
 
-namespace gpuinfo {
-	namespace gpu {
-		enum class vendor_t {
-			intel,
-			amd,
-			nvidia,
-			microsoft,
-			qualcomm,
-			unknown,
-		};
-
-		struct device_properties_t {
-			vendor_t vendor;
-			std::string name;
-			std::size_t memory_size;
-			std::size_t cache_size;
-			std::uint64_t max_frequency;
-		};
+namespace gpu {
+	enum class vendor_t {
+		intel,
+		amd,
+		nvidia,
+		microsoft,
+		qualcomm,
+		unknown,
+	};
+	struct device_properties_t {
+		vendor_t vendor;
+		std::string name;
+		std::size_t memory_size;
+		std::size_t cache_size;
+		std::uint64_t max_frequency;
+	};
 
 
-		/// Returns all GPU's properties.
-		std::vector<device_properties_t> device_properties(cl_uint device);
-	}  // namespace gpu
-} // namespace gpuinfo
+	/// Returns all GPU's properties.
+	std::vector<device_properties_t> device_properties(cl_device_id device);
+}  // namespace gpu
+
