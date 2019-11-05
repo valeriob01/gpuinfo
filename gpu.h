a4363260d6921dfc0a6c6dc9c4d8a0c17c924662
valeriob01 <valerio@selnet.org>
2019-11-05 10:07:44 +0100

    Create gpu.h

---
+++ gpu.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace gpuinfo {
+	namespace gpu {
+		enum class vendor_t {
+			intel,
+			amd,
+			nvidia,
+			microsoft,
+			qualcomm,
+			unknown,
+		};
+
+		struct device_properties_t {
+			vendor_t vendor;
+			std::string name;
+			std::size_t memory_size;
+			std::size_t cache_size;
+			std::uint64_t max_frequency;
+		};
+
+
+		/// Returns all GPU's properties.
+		std::vector<device_properties_t> device_properties();
+	}  // namespace gpu
+} // namespace gpuinfo
